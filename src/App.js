6750ace6f991bc1624406fc39c9a1c308e383740
Frederick Ofori-Boadu <friedosei@gmail.com>
2020-09-21 17:23:48 +0000

    third commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-
-import "./tailwind.output.css";
-
 import Home from "./Home";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" exact component={Home} />
-      </Switch>
-    </Router>
+       <Home/>    
   );
 }
 
