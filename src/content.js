7457c1fa61ae8d41b1265d49b1e55d6ddcef1808
Frederick Ofori-Boadu <friedosei@gmail.com>
2020-09-21 17:58:56 +0000

    phone fix

--- src/content.js
+++ src/content.js
@@ -7,14 +7,14 @@ class Content extends Component {
     return (
       <button
         id="btn"
-        className=" w-32 h-32 md:w-300 md:h-300 flex align-middle m-auto rounded-full justify-center focus:outline-none px-4 color bg-white"
+        className=" w-40 h-40 md:w-300 md:h-300 flex align-middle m-auto rounded-full justify-center focus:outline-none px-4 color bg-white"
         onClick={this.props.onClickHandler}
       >
         <h1
           className={
             this.props.data.key === 0
               ? "text-black m-auto text-3xl md:text-6xl whitespace-normal"
-              : "text-black m-auto text-xs md:text-3xl whitespace-normal "
+              : "text-black m-auto text-sm md:text-3xl whitespace-normal "
           }
         >
           {this.props.data.text}
